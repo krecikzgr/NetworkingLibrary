ef6545e6338ec354b77a84bd1ad3ddab9d822c19
Adrian Kaleta <kaleciarz@gmail.com>
2020-11-29 19:25:04 +0100

    Initial Commit

---
+++ NetworkingLibrary/NetworkingLibrary.h
@@ -0,0 +1,18 @@
+//
+//  NetworkingLibrary.h
+//  NetworkingLibrary
+//
+//  Created by Adrian Kaleta on 29/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingLibrary.
+FOUNDATION_EXPORT double NetworkingLibraryVersionNumber;
+
+//! Project version string for NetworkingLibrary.
+FOUNDATION_EXPORT const unsigned char NetworkingLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingLibrary/PublicHeader.h>
+
+
